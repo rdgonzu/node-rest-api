e53a7daeac8a105c24a0c2ed74dced8ce3b2e51b
René González <rdgonzu@gmail.com>
2021-05-03 23:13:04 -0400

    Creating Article model.

---
+++ models/article.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ArticleSchema = Schema({
+    title: String,
+    content: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    image: String
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
+
+//NOTE: MongoDB automatically pluralizes model's name. It creates "articles" collection.
